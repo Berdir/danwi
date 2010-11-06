7d5cf7bcba2191b339d46a157df6b6d7e3f2999e
Sascha Grossenbacher <saschagros@gmail.com>
2010-11-06 01:39:08 +0100

    Minor changes

--- KNN_Uebung2_A4/Aufgabe.cs
+++ KNN_Uebung2_A4/Aufgabe.cs
@@ -9,30 +9,24 @@ namespace KNN_Uebung2_A4
 		{
 			InputNode x1 = new InputNode("x1", 1);
 			InputNode x2 = new InputNode("x2", 1);
-			
 			Node h1 = new Node("h1");
 			Node h2 = new Node("h2");
-			
 			OutputNode y = new OutputNode("y");
-			
 			h1.link(1.5);
 			h2.link(0.5);
-			
 			h1.link(x1, -1);
 			h1.link(x2, -1);
 			h2.link(x1, -1);
 			h2.link(x2, -1);
-			
 			y.link(-0.5);
 			y.link(h1, 1);
 			y.link(h2, -1);
-			
 			bool converged = false;
 			int iterations = 0;
 			while (!converged && iterations < 10) {
 				Console.WriteLine("======== ITERATION {0} =========", ++iterations);
 				y.forwardCalculate();
-				converged = y.backwardSweep(1, 0.8, 0);
+				converged = y.backwardSweep(1, 0.3, 0);
 			}
 		}
 	}
